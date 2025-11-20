50b3658e185a0f850b23e7c15386330df432e06d
Uchitachoksi <uchitachoksi@gmail.com>
2025-11-19 16:19:30 -0800

    git user x project

---
+++ src/appiumtc.java
@@ -0,0 +1,12 @@
+
+public class appiumtc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("appium test case 1 ");
+		System.out.println("appium test case 2 ");
+
+	}
+
+}
---
+++ src/seleniumtc1.java
@@ -0,0 +1,12 @@
+
+public class seleniumtc1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("test case1 done");
+		System.out.println("test case 2 done");
+
+	}
+
+}
